0d2f712c8ce9fb66a7a4fdc897c7f14be197c5dd
MErlanggaa <muhammaderlanggaputraw@gmail.com>
2023-09-08 08:02:44 +0700

    Suhu

--- Suhu/script.js
+++
@@ -1,5 +0,0 @@
-function hitung() {
-    const inputForm = document.getElementById("input_form").value;
-    const hasilPerhitungan = (parseFloat(inputForm) - 32) * 5 / 9;
-    document.getElementById("hasil").innerText = "" + hasilPerhitungan.toFixed(2);
-  }
